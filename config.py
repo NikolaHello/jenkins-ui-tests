f1b30f594497baf5a8e15d573fc72b59ad27c4cb
NikolaHello <NikolaHello@yandex.com>
2026-07-16 22:18:09 +0300

    Add debug config

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ load_dotenv()
 JENKINS_URL = os.getenv("JENKINS_URL")
 JENKINS_USER = os.getenv("JENKINS_USER")
 JENKINS_PASSWORD = os.getenv("JENKINS_PASSWORD")
+
+print("URL:", JENKINS_URL)
+print("USER:", JENKINS_USER)
+print("PASSWORD:", JENKINS_PASSWORD)
